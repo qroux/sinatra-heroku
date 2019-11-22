f8cae0363d4e856b5b88cd8ffa98a1163f1bd918
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-11-22 14:56:05 +0100

    reloader working

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # Gemfile
 source 'https://rubygems.org'
 
-gem 'sinatra'
+gem 'sinatra', '~> 2.0', '>= 2.0.7'
+gem 'sinatra-contrib', '~> 2.0', '>= 2.0.7'
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     erb :index
   end
