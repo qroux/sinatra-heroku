72f83ce37c861acb5c2ae45405c5dee086ec4bae
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-11-22 16:25:15 +0100

    activeRecord working

--- Gemfile
+++ Gemfile
@@ -3,3 +3,12 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 2.0', '>= 2.0.7'
 gem 'sinatra-contrib', '~> 2.0', '>= 2.0.7'
+
+# Databse and ORM
+gem 'pg'    # for Postgres
+gem 'rake'  # so we can run Rake tasks
+gem 'sinatra-activerecord'    # for Active Record models
+
+group :development, :test do
+  gem 'sqlite3'
+end
--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require './models/post.rb'
 
 class App < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
+    Post.create(name: 'new post from controller')
+    @posts = Post.all
     erb :index
   end
 end
